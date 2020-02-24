c05924740c827147a5c0fc260998e7ddf60360fa
Maeva Reynaud <maeva.reynaud@hotmail.fr>
2020-02-24 19:24:19 +0100

    avancée du site

---
+++ js/music-player.js
@@ -0,0 +1,27 @@
+document.querySelector(".track-one").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="0";
+});
+
+document.querySelector(".track-two").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="calc(108% - 342px)";
+});
+
+document.querySelector(".track-three").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="calc(123% - 342px)";    
+});
+
+document.querySelector(".track-four").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="calc(137% - 342px)";
+});
+
+document.querySelector(".track-five").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="calc(151% - 342px)";
+});
+
+document.querySelector(".track-six").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="calc(166% - 342px)";    
+});
+
+document.querySelector(".track-seven").addEventListener("click" , function () {
+    document.querySelector(".musics span").style.top="calc(86%)";    
+});
--- js/sideBar.js
+++ js/sideBar.js
@@ -5,20 +5,49 @@ document.querySelector(".part1").addEventListener("click" , function () {
     document.querySelector(".part2 path").style.fill="#ffffff";
     document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#ffffff";
     document.querySelector(".part3 path").style.fill="#ffffff";
+    document.querySelector(".part4 path").style.fill="#ffffff";
 });
 
 document.querySelector(".part2").addEventListener("click" , function () {
-    document.querySelector(".selector").style.top="calc(50vh - 20px - 25px - 10px)";
+    document.querySelector(".selector").style.top="calc(50vh - 20px - 25px - 55px)";
     document.querySelector(".part2 path").style.fill="#4656EA";
     document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#ffffff";
     document.querySelector(".part1 path").style.fill="#ffffff";
     document.querySelector(".part3 path").style.fill="#ffffff";
+    document.querySelector(".part4 path").style.fill="#ffffff";
 });
 
 document.querySelector(".part3").addEventListener("click" , function () {
-    document.querySelector(".selector").style.top="calc(50vh - 20px - 25px - 10px + 135px)";
+    document.querySelector(".selector").style.top="calc(50vh - 20px - 25px - 10px + 131px)";
     document.querySelector(".part3 path").style.fill="#4656EA";
     document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#4656ea";
     document.querySelector(".part2 path").style.fill="#ffffff";
     document.querySelector(".part1 path").style.fill="#ffffff";
+    document.querySelector(".part4 path").style.fill="#ffffff";
 });
+
+document.querySelector(".part4").addEventListener("click" , function () {
+    document.querySelector(".selector").style.top="calc(50vh - 13px)";
+    document.querySelector(".part2 path").style.fill="#ffffff";
+    document.querySelector(".part1 path").style.fill="#ffffff";
+    document.querySelector(".part3 path").style.fill="#ffffff";
+    document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#ffffff";
+    document.querySelector(".part4 path").style.fill="#4656EA";
+    document.querySelector(".part4 g:nth-of-type(2) path").style.fill="#4656ea";
+});
+
+
+//smooth scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});
+
+
+
