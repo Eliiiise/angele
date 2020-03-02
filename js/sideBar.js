48da3c4285dcfa11d7b551a0071ee6ac35d9d3e3
Eliiiise <balvalloire@gmail.com>
2020-03-02 21:07:04 +0100

    responsif

--- js/sideBar.js
+++ js/sideBar.js
@@ -15,7 +15,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //Fill d'ariane
 
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+    if (window.innerWidth<700){
+        myFunctionResp();
+    }
+    else {
+        myFunction()
+    }
+};
 
 function myFunction() {
     if (window.scrollY<document.querySelector('header').offsetHeight/2){
@@ -51,6 +58,43 @@ function myFunction() {
         document.querySelector(".part4 path").style.fill="#ffffff";
     }
 }
+
+function myFunctionResp() {
+    if (window.scrollY<document.querySelector('header').offsetHeight/2){
+        document.querySelector(".selector").style.left="calc(10% - 25px + 15px)";
+        document.querySelector(".part1 path").style.fill="#4656EA";
+        document.querySelector(".part2 path").style.fill="#ffffff";
+        document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#ffffff";
+        document.querySelector(".part3 path").style.fill="#ffffff";
+        document.querySelector(".part4 path").style.fill="#ffffff";
+    }
+    else if (window.scrollY<document.querySelector('header').offsetHeight+document.querySelector('#story').offsetHeight-document.querySelector('header').offsetHeight/2){
+        document.querySelector(".selector").style.left="calc((80% / 3) + 10% - 25px + 7px)";
+        document.querySelector(".part2 path").style.fill="#4656EA";
+        document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#ffffff";
+        document.querySelector(".part1 path").style.fill="#ffffff";
+        document.querySelector(".part3 path").style.fill="#ffffff";
+        document.querySelector(".part4 path").style.fill="#ffffff";
+    }
+    else if (window.scrollY<document.querySelector('header').offsetHeight+document.querySelector('#story').offsetHeight+document.querySelector('#player-container').offsetHeight-document.querySelector('header').offsetHeight/2){
+        document.querySelector(".selector").style.left="calc((80% / 3) * 2 + 10% - 25px - 7px)";
+        document.querySelector(".part2 path").style.fill="#ffffff";
+        document.querySelector(".part1 path").style.fill="#ffffff";
+        document.querySelector(".part3 path").style.fill="#ffffff";
+        document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#ffffff";
+        document.querySelector(".part4 path").style.fill="#4656EA";
+    }
+    else {
+        document.querySelector(".selector").style.left="calc(90% - 25px - 15px )";
+        document.querySelector(".part3 path").style.fill="#4656EA";
+        document.querySelector(".part3 g:nth-of-type(2) path").style.fill="#4656ea";
+        document.querySelector(".part2 path").style.fill="#ffffff";
+        document.querySelector(".part1 path").style.fill="#ffffff";
+        document.querySelector(".part4 path").style.fill="#ffffff";
+    }
+}
+
+
 
 
 
